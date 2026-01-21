706d69c1ba68dbc07546aed4220d8539031496a8
iv-png <iv-png@github.com>
2026-01-21 19:37:18 +0000

    Fixed paths for install

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'bird-tracker-v2';
+const CACHE_NAME = 'bird-tracker-v3';
 const urlsToCache = [
-  '/BirdApp/',
-  '/BirdApp/index.html',
-  '/BirdApp/style.css',
-  '/BirdApp/app.js',
-  '/BirdApp/bird-images/robin.svg',
-  '/BirdApp/bird-images/bluejay.svg',
-  '/BirdApp/bird-images/cardinal.svg',
-  '/BirdApp/bird-images/sparrow.svg',
-  '/BirdApp/bird-images/goldfinch.svg',
-  '/BirdApp/bird-images/pigeon.svg',
-  '/BirdApp/bird-images/crow.svg',
-  '/BirdApp/bird-images/woodpecker.svg',
-  '/BirdApp/bird-images/seagull.svg',
-  '/BirdApp/bird-images/parrot.svg',
-  '/BirdApp/bird-images/owl.svg',
-  '/BirdApp/bird-images/hummingbird.svg',
-  '/BirdApp/bird-images/kingfisher.svg',
-  '/BirdApp/bird-images/magpie.svg'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './bird-images/robin.svg',
+  './bird-images/bluejay.svg',
+  './bird-images/cardinal.svg',
+  './bird-images/sparrow.svg',
+  './bird-images/goldfinch.svg',
+  './bird-images/pigeon.svg',
+  './bird-images/crow.svg',
+  './bird-images/woodpecker.svg',
+  './bird-images/seagull.svg',
+  './bird-images/parrot.svg',
+  './bird-images/owl.svg',
+  './bird-images/hummingbird.svg',
+  './bird-images/kingfisher.svg',
+  './bird-images/magpie.svg'
 ];
 
 self.addEventListener('install', event => {
